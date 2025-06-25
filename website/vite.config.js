a4de5bff874b717c16f74c2f4b5184767e4ce15d
Giovanni H.S <giovannijhs1@gmail.com>
2025-06-25 19:48:13 -0400

    Prepare for GitHub Pages deploy

--- website/vite.config.js
+++ website/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// 👇 Replace 'my-portfolio' with your GitHub repo name
 export default defineConfig({
-  plugins: [react()],
-  base: '/my-portfolio/', // 👈 Required for GitHub Pages
-})
+  base: '/My-Portfolio/', // change this to your repo name
+  plugins: [react()]
+});
